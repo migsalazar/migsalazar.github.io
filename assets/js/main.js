9a3cc176d7d54a2b55866119ee90d8e2fbdf7415
Mig Salazar <migsalazar@users.noreply.github.com>
2016-05-20 10:05:04 -0500

    what about after one sec

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,12 @@ $(function(){
 
     //just for fun
     var anim = 'rubberBand';
-
-    $('h1 i.fa').removeClass('animated ' + anim)
+    setTimeout( function(){ 
+        $('h1 i.fa').removeClass('animated ' + anim)
             .addClass('animated ' + anim)
             .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
             function(){
               $(this).removeClass('animated ' + anim);
             });
+    }  , 1000 );
 });
