9279993d0027f2b84584dc623116a62ba7ea0a8c
Mig Salazar <migsalazar@users.noreply.github.com>
2016-05-20 10:08:06 -0500

    maybe on mouse move

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 $(function(){
-
     //just for fun
-    var anim = 'rubberBand';
-    setTimeout( function(){ 
-        $('h1 i.fa').removeClass('animated ' + anim)
-            .addClass('animated ' + anim)
-            .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
-            function(){
-              $(this).removeClass('animated ' + anim);
-            });
-    }  , 3000 );
+    $('body').on('mousemove', function(){
+        var anim = 'rubberBand';
+        setTimeout( function(){ 
+            $('h1 i.fa').removeClass('animated ' + anim)
+                .addClass('animated ' + anim)
+                .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+                function(){
+                  $(this).removeClass('animated ' + anim);
+                });
+        }  , 2000 );
+    });
 });
